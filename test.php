13cc18fe618e1cce0044857e34adb57f00aadd2f
danhng <d.t.nguyen@newcastle.ac.uk>
2015-11-23 01:42:37 +0000

    Set up Twig and Red Bean and did some misc tests

--- test.php
+++ test.php
@@ -1,3 +1,28 @@
+
 <?php
-phpinfo()
+// include and register Twig auto-loader
+include 'twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+try {
+// specify where to look for templates
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+
+// initialize Twig environment
+$twig = new Twig_Environment($loader);
+
+// load template
+$template = $twig->loadTemplate('hello.html');
+
+// set template variables
+// render template
+echo $template->render(array(
+'name' => 'Clark Kent',
+'username' => 'ckent',
+'password' => 'krypt0n1te',
+));
+
+} catch (Exception $e) {
+die ('ERROR: ' . $e->getMessage());
+}
 ?>
