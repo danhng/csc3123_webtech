dcdb1a31fce7d9343a3ad76c85e0ff39e1a6312c
danhng <d.t.nguyen@newcastle.ac.uk>
2015-11-26 19:17:18 +0000

    Set up the framwork

---
+++ framework/class/contact.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * A class that contains code to implement a contact page
+ *
+ * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
+ * @copyright 2012-2015 Newcastle University
+ *
+ */
+/**
+ * Support the /contact page
+ */
+    class Contact extends Siteaction
+    {
+/**
+ * Handle contact operations /contact/xxxx
+ *
+ * @param object	$context	The context object for the site
+ *
+ * @return string	A template name
+ */
+        public function handle($context)
+        {
+            if (($msg = $context->postpar('message', '')) !== '')
+            { # there is a post
+                mail(Config::SYSADMIN, $context->postpar('subject', 'No Subject'), $msg);
+                $context->local()->addval('done', TRUE);
+            }
+            return 'contact.twig';
+        }
+    }
+?>
---
+++ framework/class/robot.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * A class that contains code to handle robots.txt requests
+ *
+ * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
+ * @copyright 2015 Newcastle University
+ *
+ */
+/**
+ * Generate a robots.txt for the site
+ */
+    class Robot extends Siteaction
+    {
+/**
+ * Handle /robots.txt requests
+ *
+ * @param object	$context	The context object for the site
+ *
+ * @return string	A template name
+ */
+	public function handle($context)
+	{
+	    header('Content-Type: text/plain');
+	    return 'robot.twig';
+	}
+    }
+?>
